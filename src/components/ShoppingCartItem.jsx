d518e4936d2ce9c5b7dcd7d0b4e8dd0267b37375
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2022-06-29 22:55:41 -0500

    Eliminando productos del carrito

--- src/components/ShoppingCartItem.jsx
+++ src/components/ShoppingCartItem.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '@context/AppContext';
 import '@styles/ShoppingCartItem.scss';
 
 import close from '@icons/icon_close.png';
 
 const ShoppingCartItem = ({ product }) => {
+    const { removeFromCart } = useContext(AppContext);
+
+    const handleRemove = (item) => {
+        removeFromCart(item);
+    }
+
     return (
         <div className="ShoppingCartItem">
             <figure>
@@ -11,7 +18,7 @@ const ShoppingCartItem = ({ product }) => {
             </figure>
             <p>{product.title}</p>
             <p>${product.price}</p>
-            <img src={close} alt="close" />
+            <img src={close} alt="close" onClick={() => handleRemove(product)} />
         </div>
     );
 }
